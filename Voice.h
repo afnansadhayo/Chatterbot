8b02799e9be1e842b0a052d64d26710fb16e75c9
Afnan Sadhayo <afnan5sadhayo@gmail.com>
2019-10-19 02:11:31 +0500

    The following function textually and audibly communicates a phrase.
    The open source eSpeak speech synthesizer is used to create the audible message. If the eSpeak exe is not located in the directory, no audible message will be heard

---
+++ Voice.h
@@ -0,0 +1,13 @@
+#ifndef _VOICE_H
+#define _VOICE_H
+
+#include <iostream>
+
+using namespace std;
+
+class Voice {
+public:
+    void say(string phrase);    // Used to textually and audibly communicate a phrase
+};
+
+#endif
